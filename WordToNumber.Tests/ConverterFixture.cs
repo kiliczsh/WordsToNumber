cdf5e6d09f693f46e1363fee1f323b650a8d8548
kiliczsh <muhammeddkilicc@gmail.com>
2022-12-14 12:39:51 +0300

    Add new test cases

--- WordToNumber.Tests/ConverterFixture.cs
+++ WordToNumber.Tests/ConverterFixture.cs
@@ -5,6 +5,10 @@ namespace WordToNumber.Tests
     public class ConverterFixture
     {
         [TestCase("one hundred twenty-three thousand four hundred thirty-five", ExpectedResult = "123435")]
+        [TestCase("There are many books.", ExpectedResult = "There are many books")]
+        [TestCase(
+            "I have one trillion two hundred thirty-four billion three hundred fifty-six million seven hundred eighty-nine thousand one hundred twenty-three things to do.",
+            ExpectedResult = "I have 1234356789123 things to do")]
         public string Should_Translate_Correctly(string input)
         {
             return Converter.Translate(input);
