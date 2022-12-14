d36d13a8a46cb997c95cc7726b84284f1dceb0d4
kiliczsh <muhammeddkilicc@gmail.com>
2022-12-14 12:36:22 +0300

    Implement conversion and number extraction

--- WordToNumber/Converter.cs
+++ WordToNumber/Converter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -16,10 +17,65 @@ namespace WordToNumber
                 return new List<string>();
             }
 
+            int signatureFlag;
             var matches = Regex.Matches(words, @"\w+").ToList();
             var lowerCases = matches.Select(m => m.Value.ToLowerInvariant()).ToList();
-            // TODO: convert number words to digit by using Numbers
-            return lowerCases;
+            var isConvertable = lowerCases.Select(l => Numbers.ContainsKey(l)).ToList();
+            var list = new List<string>();
+            var sentence = new List<string>();
+            for (var index = 0; index < isConvertable.Count; index++)
+            {
+                var token = isConvertable[index];
+                if (token)
+                {
+                    list.Add(lowerCases[index]);
+                }
+                else
+                {
+                    if (list.Any())
+                    {
+                        signatureFlag = list.FirstOrDefault()!.Equals("minus") ? -1 : 1;
+                        sentence.Add(GetDigits(list.Select(n => Numbers[n]), signatureFlag).ToString());
+                        list = new List<string>();
+                    }
+
+                    sentence.Add(matches[index].Value);
+                }
+            }
+
+            if (!list.Any() || sentence.Any())
+            {
+                return sentence;
+            }
+
+            signatureFlag = list.FirstOrDefault()!.Equals("minus") ? -1 : 1;
+            sentence.Add(GetDigits(list.Select(n => Numbers[n]), signatureFlag).ToString());
+
+            return sentence;
+        }
+
+        private static long GetDigits(IEnumerable<long> numbers, int signatureFlag = 1)
+        {
+            var percentile = 0L;
+            var result = 0L;
+            foreach (var number in numbers)
+            {
+                switch (number)
+                {
+                    case >= 1000:
+                        result += percentile * number;
+                        percentile = 0;
+                        break;
+                    case >= 100:
+                        percentile *= number;
+                        break;
+                    default:
+                        percentile += number;
+                        break;
+                }
+            }
+
+            return (result + percentile) * signatureFlag;
         }
 
         private static readonly Dictionary<string, long> Numbers = new()
